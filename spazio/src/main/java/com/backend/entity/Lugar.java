f2ad0afda1ebe47ca2e9971246eaa19f485fd43c
valentingm1 <gomarchjjba@gmail.com>
2024-05-08 10:35:10 -0300

    Inicio del proyecto

---
+++ spazio/src/main/java/com/backend/entity/Lugar.java
@@ -0,0 +1,40 @@
+package com.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "LUGARES")
+public class Lugar {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public Lugar(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ spazio/src/test/java/com/example/spazio/SpazioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spazio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpazioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
